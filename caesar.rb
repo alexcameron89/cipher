3be4dff2d5150740277be261ffa0fe57b4ccdfb8
alexcameron89 <alexcameron98@gmail.com>
2014-08-02 12:50:00 -0500

    Refactors duplicate code into method 'run_cipher'

--- caesar.rb
+++ caesar.rb
@@ -6,6 +6,8 @@ class Caesar
 		@index = { "a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5, "f" => 6, "g" => 7, "h" => 8, "i" => 9, "j" => 10, "k" => 11, "l" => 12, "m" => 13, "n" => 14, "o" => 15, "p" => 16, "q" => 17, "r" => 18, "s" => 19, "t" => 20, "u" => 21, "v" => 22, "w" => 23, "x" => 24, "y" => 25, "z" => 26 }
 	end
 
+
+	# Could Encrypt & Decrypt work without shifting the dictionary? Add to @index value instead?
 	def shift_dictionary(key = @key)
 		shift = @dictionary.slice!(0..(key - 1))
 		@dictionary = @dictionary << shift
@@ -13,23 +15,13 @@ class Caesar
 
 	def encrypt
 		shift_dictionary
-		@message = @message.split("")
-		@message.each do |letter|
-			letter.replace(@dictionary[@index[letter]-1]) if letter.match(/[a-z]/)
-		end
-		@message = @message.join("")
-		@message
+		run_cipher
 	end
 
 	def decrypt
 		@message.downcase!
 		shift_dictionary(26-(@key * 2))
-		@message = @message.split("")
-		@message.each do |letter|
-			letter.replace(@dictionary[@index[letter]-1]) if letter.match(/[a-z]/)
-		end
-		@message = @message.join("")
-		@message
+		run_cipher
 	end
 
 	def run_cipher
@@ -40,5 +32,10 @@ class Caesar
 		end
 		@message = @message.join("")
 		@message		
+	end
+
+	def change_key(new_key)
+		@key = new_key
+		puts "The shift key is now #{@key}"
 	end
 end
