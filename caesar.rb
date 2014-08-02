e610a96d0e214d6f6c17b43016392c6b034d59ff
alexcameron89 <alexcameron98@gmail.com>
2014-08-02 15:38:12 -0500

    Refactor: Takes out unnecessary backtracking with '-1's.

--- caesar.rb
+++ caesar.rb
@@ -3,13 +3,13 @@ class Caesar
 		@message = message.downcase
 		@key = key
 		@dictionary = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-		@index = { "a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5, "f" => 6, "g" => 7, "h" => 8, "i" => 9, "j" => 10, "k" => 11, "l" => 12, "m" => 13, "n" => 14, "o" => 15, "p" => 16, "q" => 17, "r" => 18, "s" => 19, "t" => 20, "u" => 21, "v" => 22, "w" => 23, "x" => 24, "y" => 25, "z" => 26 }
+		@index = { "a" => 0, "b" => 1, "c" => 2, "d" => 3, "e" => 4, "f" => 5, "g" => 6, "h" => 7, "i" => 8, "j" => 9, "k" => 10, "l" => 11, "m" => 12, "n" => 13, "o" => 14, "p" => 15, "q" => 16, "r" => 17, "s" => 18, "t" => 19, "u" => 20, "v" => 21, "w" => 22, "x" => 23, "y" => 24, "z" => 25 }
 	end
 
 
 	# Could Encrypt & Decrypt work without shifting the dictionary? Add to @index value instead?
 	def shift_dictionary(key = @key)
-		shift = @dictionary.slice!(0..(key - 1))
+		shift = @dictionary.slice!(0...(key))
 		@dictionary = @dictionary << shift
 	end
 
@@ -28,7 +28,7 @@ class Caesar
 		@message.downcase!
 		@message = @message.split("")
 		@message.each do |letter|
-			letter.replace(@dictionary[@index[letter]-1]) if letter.match(/[a-z]/)
+			letter.replace(@dictionary[@index[letter]]) if letter.match(/[a-z]/)
 		end
 		@message = @message.join("")
 		@message		
